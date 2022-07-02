1798f0edec78132c316951475607f028961db85a
mature <1153706397@qq.com>
2022-07-02 17:04:13 +0800

    自动配置hysky

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,21 @@
   <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
 </template>
 
+
+
 <script lang="ts">
 import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from
+'./components/HelloWorld.vue'
 
-export default defineComponent({
+export default
+
+defineComponent({
   name: 'App',
+
+
+
+
   components: {
     HelloWorld
   }

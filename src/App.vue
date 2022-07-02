e04c7f133b43231b0769d5101ea1bfbcc19264af
mature <1153706397@qq.com>
2022-07-02 17:14:05 +0800

    利用hysky修改代码格式

--- src/App.vue
+++ src/App.vue
@@ -3,20 +3,12 @@
   <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
 </template>
 
-
-
 <script lang="ts">
 import { defineComponent } from 'vue'
-import HelloWorld from
-'./components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue'
 
-export default
-
-defineComponent({
+export default defineComponent({
   name: 'App',
-
-
-
 
   components: {
     HelloWorld

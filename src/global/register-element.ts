5c579367d6b151a73c0d96dec217bff2ae148d5a
mature <1153706397@qq.com>
2022-07-10 17:11:39 +0800

    feat(service): 切换环境方式1：手动切换

--- src/global/register-element.ts
+++ src/global/register-element.ts
@@ -2,6 +2,7 @@ import 'element-plus/dist/index.css'
 // import 'element-plus/theme-chalk/el-button.css'
 import { ElButton, ElForm, ElInput, ElRadio } from 'element-plus'
 const components = [ElButton, ElForm, ElInput, ElRadio]
+import { App } from 'vue'
 
 export default function (app: App): void {
   for (const component of components) {
---
+++ src/service/request/config.ts
@@ -0,0 +1,9 @@
+// 1.切换环境-手动更改
+const BASE_URL = 'http://tao.org/dev'
+const BASE_NAME = 'dev'
+
+// const BASE_URL = 'http://tao.org/prod'
+// const BASE_NAME = 'prod'
+
+// const BASE_URL = 'http://tao.org/test'
+// const BASE_NAME = 'test'
